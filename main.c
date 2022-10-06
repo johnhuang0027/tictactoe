dde474481afad951c33c0ff794c29cb07b8561a5
johnhuang0027 <112910208+johnhuang0027@users.noreply.github.com>
2022-10-05 19:24:47 -0700

    outline for game

---
+++ main.c
@@ -0,0 +1,17 @@
+//Tic Tac Toe game between 2 players or player vs computer
+int main(){
+
+    //prompt user for game they wish to play
+
+    //read information from console
+
+    //print board
+
+    //read information from console
+
+    //keep updating board until player either wins, loses, or ties
+    
+
+
+    return 0;
+}
