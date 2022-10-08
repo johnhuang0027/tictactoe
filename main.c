d6a6bb170404b77551c564d4801037dc73bb14ac
johnhuang0027 <112910208+johnhuang0027@users.noreply.github.com>
2022-10-07 18:00:19 -0700

    added printboard

--- main.c
+++ main.c
@@ -1,12 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void printBoard();
+
 //Tic Tac Toe game between 2 players or player vs computer
 int main(){
 
+    char str[100];
+    int menuOption;
     //prompt user for game they wish to play
+    printf("Welcome to TicTacToe.\n1---person vs person\n2---person vs random computer\nEnter your choice 1 or 2\n");
 
     //read information from console
+    scanf("%d",&menuOption);
+    while(menuOption !=1 && menuOption != 2){
+        printf("Invalid input, please try again\n");
+        scanf("%d",&menuOption);
+    }
 
     //print board
+    if(menuOption==1){
+        int x;
+        int y;
+        printBoard();
 
+        printf("Player 1 move:\n");
+        scanf("%d %d", &x,&y);
+    }
+    
     //read information from console
 
     //keep updating board until player either wins, loses, or ties
@@ -14,4 +36,15 @@ int main(){
 
 
     return 0;
+}
+
+void printBoard(){
+    printf("+-------------+\n");
+    printf("|  |  |  |\n");
+    printf("+-------------+\n");
+    printf("|  |  |  |\n");
+    printf("+-------------+\n");
+    printf("|  |  |  |\n");
+    printf("+-------------+\n");
+
 }
